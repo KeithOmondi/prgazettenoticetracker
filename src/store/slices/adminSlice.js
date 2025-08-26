51c3a66cabcd31bd34171efa37c233c4cc53e7ff
KeithOmondi <denniskeith62@gmail.com>
2025-08-26 18:06:45 +0300

    commit

--- src/store/slices/adminSlice.js
+++ src/store/slices/adminSlice.js
@@ -2,8 +2,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const ADMIN_API = "http://localhost:8000/api/v1/records";
-const BULK_API = "http://localhost:8000/api/v1/bulk";
+const ADMIN_API = "https://kenyagazette-1.onrender.com/api/v1/records";
+const BULK_API = "https://kenyagazette-1.onrender.com/api/v1/bulk";
 
 // ==================== THUNKS ====================
 
